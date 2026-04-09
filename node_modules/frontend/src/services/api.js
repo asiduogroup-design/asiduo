355218ea8897364c527f0d75e95f65f5dd9f0f50
uma <udayarekhachowdary96@gmail.com>
2026-04-09 12:34:30 +0530

    Remove unintended cache and mirror file changes

--- node_modules/frontend/src/services/api.js
+++ node_modules/frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    process.env.REACT_APP_API_BASE_URL || "https://asiduo.onrender.com/api",
+  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json"
   }
